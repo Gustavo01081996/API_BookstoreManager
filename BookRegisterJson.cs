5df589a99cb85f40bb8a7630f73f8ab914aa1028
Gustavo de Assis Vilarino <132522283+Gustavo01081996@users.noreply.github.com>
2024-09-25 09:14:05 -0300

    Inserção de arquivos de request

---
+++ BookRegisterJson.cs
@@ -0,0 +1,18 @@
+﻿namespace BookstoreManager.Requests;
+
+public enum GenderBook
+{
+    fiction,
+    romance, 
+    mystery
+}
+public class BookRegisterJson
+{
+    public int Id;
+    public string Title { get; set; } = string.Empty;   
+    public double Price { get; set; }   
+    public int QuantityInStock { get; set; }
+    public GenderBook Gender { get; set; }
+
+
+}
---
+++ BookUpdateBookJson.cs
@@ -0,0 +1,10 @@
+﻿namespace BookstoreManager.Requests;
+
+public class BookUpdateBookJson
+{
+    public string Title { get; set; } = string.Empty;
+    public double Price { get; set; }
+    public int QuantityInStock { get; set; }
+
+    public GenderBook Gender;
+}
